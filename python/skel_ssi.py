352a7e21d40ad46b740e31aec180b4bcb1fb1e1a
Arnab Paikaray <42674892+arnabGudu@users.noreply.github.com>
2022-01-21 16:14:09 +0530

    skel_ssi.py modified

--- python/skel_ssi.py
+++ python/skel_ssi.py
@@ -1,94 +1,96 @@
+import os
 import csv
 import argparse
 
-class Joint:
-    def __init__(self, name, x, y, z):
-        self.name = name
-        self.x = float(x)
-        self.y = float(y)
-        self.z = float(z)
-    
-    def __str__(self):
-        return self.name + ": " + str(self.x) + ", " + str(self.y) + ", " + str(self.z)
-
-    def __repr__(self):
-        return self.__str__()
-
-    def __eq__(self, other):
-        return self.name == other.name and self.x == other.x and self.y == other.y and self.z == other.z
 
 class Skeleton:
-    def __init__(self):
-        self._joints = dict()
-        self.joint_names = ['Head', 'Neck', 'Torso', 'HipCenter', 
+    def __init__(self) -> None:
+        self.__joints = dict()
+        self.joint_names = ['Head', 'Neck', 'Torso', 'HipCenter',
                             'LShoulder', 'LElbow', 'LWrist', 'LHand',
                             'RShoulder', 'RElbow', 'RWrist', 'RHand',
                             'LHip', 'LKnee', 'LAnkle', 'LFoot',
                             'RHip', 'RKnee', 'RAnkle', 'RFoot',
-                            'ShoulderSpine', 
+                            'ShoulderSpine',
                             'LHandTip', 'LThumb', 'RHandTip', 'RThumb']
 
-    def __getitem__(self, key):
-        return self._joints[key]
+    def __getitem__(self, key: str) -> tuple:
+        if key in self.__joints:
+            return self.__joints[key]
+        else:
+            raise KeyError("Invalid joint name: " + key)
 
-    def __setitem__(self, key, value):
-        self._joints[key] = value
+    def __setitem__(self, key: str, value: tuple or list) -> None:
+        if key in self.joint_names:
+            self.__joints[key] = value
+        else:
+            raise KeyError("Invalid joint name: " + key)
 
-    def __len__(self):
-        return len(self._joints)
+    def __len__(self) -> int:
+        return len(self.__joints)
 
-    def __iter__(self):
-        return iter(self._joints)
+    def __iter__(self) -> iter:
+        return iter(self.__joints)
 
-    def __contains__(self, item):
-        return item in self._joints
+    def __contains__(self, item: str) -> bool:
+        return item in self.__joints
+
+    def __str__(self) -> str:
+        return ', '.join(['{}: {}'.format(k, v) for k, v in self.__joints.items()])
+
+    def __repr__(self) -> str:
+        return str(self)
 
 class KinectStream:
-    def __init__(self, file_name):
-        self._file_name = file_name
-        self._stream = []
-        self._read_file()
+    def __init__(self, file_name: str) -> None:
+        self.__file_name, self.__extension = os.path.splitext(file_name)
+        self.__stream = []
+        self.__read_file()
 
-    def _read_file(self):
-        with open(self._file_name, 'r') as csv_file:
+    def __read_file(self) -> None:
+        with open(self.__file_name + self.__extension, 'r') as csv_file:
             reader = csv.reader(csv_file, delimiter=';')
             for row in reader:
                 sk = Skeleton()
                 for i, joint_name in enumerate(sk.joint_names):
-                    sk[joint_name] = Joint(joint_name, row[i * 14], row[i * 14 + 1], row[i * 14 + 2])
-                self._stream.append(sk)
+                    sk[joint_name] = (float(row[i * 14]), float(row[i * 14 + 1]), float(row[i * 14 + 2]))
+                self.__stream.append(sk)
 
-    def write_file(self):
-        with open(self._file_name + '_', 'w') as csv_file:
+    def write_file(self) -> None:
+        with open(self.__file_name + '_processed' +self.__extension, 'w', newline='', encoding='utf-8') as csv_file:
             writer = csv.writer(csv_file, delimiter=',')
-            for sk in self._stream:
+            row = []
+            for joint_name in self.__stream[0].joint_names:
+                row.extend([joint_name+'X', joint_name+'Y', joint_name+'Z'])
+            writer.writerow(row)
+
+            for sk in self.__stream:
                 row = []
                 for joint_name in sk.joint_names:
-                    row.append(sk[joint_name].x)
-                    row.append(sk[joint_name].y)
+                    row.append(sk[joint_name][0])
+                    row.append(sk[joint_name][1])
+                    row.append(sk[joint_name][2])
+                writer.writerow(row)
 
-    def animation(self):
-        for sk in self._stream:
-            for joint_name in sk.joint_names:
-                print(sk[joint_name])
+    def __str__(self) -> str:
+        return '\n\n'.join(['{}: {}'.format(i, sk) for i, sk in enumerate(self.__stream)])
 
-    def __str__(self):
-        return str(self._stream)
-
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
 
-    def __len__(self):
-        return len(self._stream)
+    def __len__(self) -> int:
+        return len(self.__stream)
 
-    def __iter__(self):
-        return iter(self._stream)
+    def __iter__(self) -> iter:
+        return iter(self.__stream)
 
-    def __contains__(self, item):
-        return item in self._stream
-            
+    def __contains__(self, item) -> bool:
+        return item in self.__stream
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('file', nargs='?', type=str, help='file to read')
+    parser.add_argument('file', nargs='?', type=str, help='skeleton_ssi.csv file from nova software')
     args = parser.parse_args()
+
     ks = KinectStream(args.file)
+    ks.write_file()
